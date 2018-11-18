beebbf0a95af8de81e45036636c8528b8244eb1a
liukangjing <liukangjing@163.com>
2018-11-18 20:39:12 +0800

    新闻列表数据展示

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,4 +1,4 @@
-from flask import session, render_template, current_app, jsonify
+from flask import session, render_template, current_app, jsonify, request
 # 2-----导入蓝图对象，使用蓝图对象
 from info.utils.response_code import RET
 from . import news_blue
@@ -71,6 +71,76 @@ def index():
 
     return render_template('news/index.html', data=data)
 
+@news_blue.route('/news_list')
+def news_list():
+    """
+    首页新闻列表
+    获取参数－－检查参数－－业务处理－－返回结果
+    1. 获取参数　get请求获取  cid   page   per_page
+    2.                  cid 分类id　　
+    2. 检查参数，转化参数的数据类型
+    cid, page, per_page = int(cid), int(page), int(per_page)
+    3. 根据分类id查询新闻列表，新闻列表默认按照新闻发布时间倒序排序
+    4. 判断用户选择的新闻分类
+    实现形式一
+    if cid> 1:
+        News.query.fliter(news, category_id==cid).order_by(News.create_time.desc()).pageinait(page, perpage, False)
+    else:
+        News.query.filter().order_by(News.create_time.desc()).pageinait(page, perpage, False)
+
+    实现形式二
+    fliers = []
+    if cid > 1:
+        filters.append(News.category_id==cid)
+    # 拆包
+        News.query.filter(*filters).order_by(News.create_time.desc()).pageinait(page, perpage, False)
+    5. 获取分页后的数据
+    paginate.items 分页总数据
+    paginate.pages 分页总页数
+    pageinate.page 分页当前页数
+    6. 遍历分页后的总数据，调用模型类中的方法，转成字典
+    7. 返回结果
+
+    :return:
+    """
+    # 获取参数
+    cid = request.args.get('cid', '1')
+    page = request.args.get('page', '1')
+    per_page = request.args.get('per_page', '10')
+    # 检查参数
+    try:
+        cid.page.per_page = int(cid), int(page), int(per_page)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数类型错误")
+    # 根据分类id查询数据库
+    filters = []
+    # 如果分类id不是最新, 添加分类id给filter过滤条件
+    if cid > 1:
+        filters.append(News.category_id == cid)
+    # 查询数据库
+    try:
+        # 拆包
+        paginate = News.query.filter(*filters).order_by(News.create_time.desc()).pageinait(page, per_page, False)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询新闻列表失败")
+    # 使用分页对象，　获取分页后的数据，总数据，总页数，当前页数
+    news_list = paginate.items
+    total_page = paginate.pages
+    current_page = paginate.page
+    # 遍历分页数据，转成字典
+    news_dict_list = []
+    for news in news_list:
+        news_dict_list.append(news.to_dict())
+    # 定义容器　返回数据
+    data = {
+        'total_page': total_page,
+        'current_page': current_page,
+        'news_dict_list': news_dict_list
+    }
+    # 返回数据
+    return jsonify(errno=RET.Ok, errmsg="OK")
 
 # 加载logo图标，浏览器会默认请求,url地址：http://127.0.0.1:5000/favicon.ico
 # flask 默认创建一个静态路由
